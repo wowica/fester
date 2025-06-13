9c96d2e6e756efa6209663253f38b4b1e6151efb
Carlos Souza <carloshrsouza@gmail.com>
2025-06-13 17:51:46 -0400

    Silence logs a little

--- config/dev.exs
+++ config/dev.exs
@@ -54,7 +54,7 @@ config :fester, FesterWeb.Endpoint,
 config :fester, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$level] $message\n", level: :info
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
